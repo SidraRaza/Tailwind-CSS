499ab3d02ba1052befe39a8dd63985a86c8a1a37
SidraRaza <160983959+SidraRaza@users.noreply.github.com>
2024-10-21 15:07:33 +0500

    first commit

--- custimization/src/app/components/About.tsx
+++ custimization/src/app/components/About.tsx
@@ -3,13 +3,17 @@ import Image from 'next/image';
 
 const Page = () => {
   return ( 
-    <div className="max-w-[1320px] mx-auto md:py-[80] py-5 flex lg:pl-8 pt-10 flex-col sm:flex-row items-center text-white min-h-[550px] bg-yellow ">
+    <div className="max-w-[1320px] mx-auto md:py-[80] py-5 flex lg:pl-8 pt-10 flex-col sm:flex-row  items-center text-white min-h-[550px] bg-yellow ">
     <div className="basis-[30%] hover:scale-105 duration-300 border-green-500 ml-8">
       <Image src="/me2.jpg" alt="About Us" className="w-full rounded-full hover:shadow-2xl shadow-orange-500" width={300} height={300} />
     </div>
-    <div className="basis-[60%] px-5">
-      <h1 className="text-4xl text-center text-black  hover:underline underline-offset-8 underline-purple-800 font-bold">About Us</h1>
-      <p className="sm:text-lg md:text-1xl text-black lg:ml-12 ml-4 pt-4 md:pt-10 text-justify">
+    <div className="basis-[60%] px-5 lg-mt-0 mt-10">
+    <div className="relative lg:text-3xl text-xl text-center text-black font-bold">
+  <h2 className="relative z-10 inline-block bg-green px-4 py-2 -skew-y-3 font-serif">
+    About Us
+  </h2>
+</div>
+      <p className="sm:text-lg ss:text-md md:text-1xl text-black lg:ml-12 ml-4 pt-4 md:pt-10 text-justify font-serif">
        &quot;Hello! I&apos;m a web developer and future Generative Cloud AI Engineer with a passion for building cutting-edge solutions. 
        With expertise in web development and a keen interest in AI and cloud technology, I&apos;m always looking to push the boundaries
        of what&apos;s possible. Plus, I&apos;m also a graphic designer, bringing a creative touch to my work. Let&apos;s explore the intersection 
--- custimization/tailwind.config.ts
+++ custimization/tailwind.config.ts
@@ -15,13 +15,32 @@ const config: Config = {
         yellowLight: '#FEE13E',
       },
       backgroundImage: {
-        'hero-pattern': "url('/bg2.webp')",
+        'image': "url('/bg2.webp')",
+        'aboutImage': "url('/me2.jpg')",
        
+      },
+      Image:{
+        'aboutImage': "url('/me2.jpg')",
+      },
+      fontSize: {
+        sm: ['14px', '20px'],
+        base: ['16px', '24px'],
+        xl: '1.25rem',
+        '2xl': '1.563rem',
+        '3xl': '1.953rem',
+        '4xl': '2.441rem',
+        '5xl': '3.052rem',
+      },
+      margin: {
+        '125px': '125px',
       },
       fontFamily: {
         'serif': ['ui-serif', 'Georgia'],
         'display': ['Oswald', ],
-      }
+      },
+      screens: {
+        'ss': '320px',
+      },
     },
   },
   plugins: [],
